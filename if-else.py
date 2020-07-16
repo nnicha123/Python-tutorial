e1ab2c76f216f72d7a32c1bba8b446b3cd702ce9
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-16 10:11:00 +0700

    added beginners' tutorial files

---
+++ if-else.py
@@ -0,0 +1,16 @@
+# age = int(input('Enter your age:'))
+# if age < 10:
+#     #code block
+#     print('You are young, strange one')
+# elif age < 40:
+#     print('You are younger than 40')
+# else :
+#     print('You are wise beyond doubt')
+
+meaty = input('Do you eat meat? (y/n):')
+
+if meaty != 'y':
+    print('Here is the veggie menu...')
+else:
+    print('Here is the meaty menu...')
+
---
+++ string_format.py
@@ -0,0 +1,8 @@
+num1 = 3.1425467389
+num2 = 20.3903948
+
+# print('num 1 is',num1, "and num 2 is",num2)
+# FORMAT
+print('num1 is {0:.3f} and num2 is {1:.3f}'.format(num1,num2))
+# USING F-STRINGS
+print(f'num1 is {num1:.3f} and num2 is {num2:.3f}')
