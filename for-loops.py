173071fc4d14f3ce9a8360dcaa6d6653949ce33d
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-16 10:37:30 +0700

    added 3 more exercises

---
+++ for-loops.py
@@ -0,0 +1,12 @@
+# For loop
+ninjas = ['ryu','crystal','yoshi','ken']
+
+# for ninja in ninjas[1:3]:
+#     print(ninja)
+
+for ninja in ninjas:
+    if ninja == 'yoshi':
+        print(f'{ninja} - black belt')
+        # break
+    else:
+        print(ninja)
