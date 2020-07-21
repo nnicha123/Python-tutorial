eb0a97c461fae4d8bfa179921744c30fd8bbd9ef
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-21 10:16:21 +0700

    finish tic tac toe challenge 3

--- tic-tac-toe4.py
+++ tic-tac-toe4.py
@@ -1,3 +1,5 @@
+import math
+print('Enter cells:')
 in1 = input()
 in2 = list(in1)
 test1 = in2[0:3]
@@ -8,44 +10,62 @@ str2 = '| ' + ' '.join(test2) + ' |'
 str3 = '| ' + ' '.join(test3) + ' |'
 
 totalStr = '---------\n' + str1 + '\n' + str2 + '\n' + str3 + '\n' + '---------'
-print(totalStr)
+
 newstr = []
 newstr.append(test1) 
 newstr.append(test2)
 newstr.append(test3)
-# print(newstr)
-# # Vertical
-# print(newstr[0][0])
-# print(newstr[1][0])
-# print(newstr[2][0])
 
-# print(newstr[0][1])
-# print(newstr[1][1])
-# print(newstr[2][1])
+# Check win
+horizonX = 0
+horizonO = 0
+Xwin = 0
+Owin = 0
+countX = 0
+countO = 0
+for i in range(0,3):
+    horizonX = 0
+    horizonO = 0
+    for j in range(0,3):
+        if newstr[i][j] == 'X':
+            horizonX += 1
+            countX += 1
+            if horizonX == 3:
+                Xwin = 1
+        elif newstr[i][j] == 'O':
+            horizonO += 1
+            countO += 1
+            if horizonO == 3:
+                Owin = 1
+# Vertical win
+verX = 0
+verO = 0
+for i in range(0,3):
+    verX = 0
+    verO = 0
+    for j in range(0,3):
+        if newstr[j][i] == 'X':
+            verX += 1
+            if verX == 3:
+                Xwin += 1
+        elif newstr[j][i] == 'O':
+            verO += 1
+            if verO == 3:
+                Owin += 1
 
-# print(newstr[0][2])
-# print(newstr[1][2])
-# print(newstr[2][2])
+if (newstr[0][0] == 'X' and newstr[1][1] == 'X' and newstr[2][2] == 'X') or (newstr[0][2] == 'X' and newstr[1][1] == 'X' and newstr[2][0] == 'X'):
+    Xwin += 1
+elif (newstr[0][0] == 'O' and newstr[1][1] == 'O' and newstr[2][2] == 'O') or (newstr[0][2] == 'O' and newstr[1][1] == 'O' and newstr[2][0] == 'O'):
+    Owin += 1
 
-# # Horizontal
-# print(newstr[0][0])
-# print(newstr[0][1])
-# print(newstr[0][2])
-
-# print(newstr[1][0])
-# print(newstr[1][1])
-# print(newstr[1][2])
-
-# print(newstr[2][0])
-# print(newstr[2][1])
-# print(newstr[2][2])
-
-# Diagonal
-# print(newstr[0][0])
-# print(newstr[1][1])
-# print(newstr[2][2])
-
-# print(newstr[0][2])
-# print(newstr[1][1])
-# print(newstr[2][0])
-# print(list(test1+test2+test3))
+print(totalStr)
+if (Xwin == 1 and Owin == 1) or math.fabs(countX - countO) >=2:
+    print('Impossible')
+elif Xwin == 1:
+    print('X wins')
+elif Owin == 1:
+    print('O wins')
+elif countX + countO < 9:
+    print('Game not finished')
+elif Xwin == Owin:
+    print('Draw')
